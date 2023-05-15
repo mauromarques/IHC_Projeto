4176092452121a3ccd4057681d50ccf158e631fc
Inês Santos <ismsantos@ua.pt>
2023-05-15 22:17:48 +0100

    added calendar and profile screens

--- Unity/app/src/main/java/com/example/u_nity/CalendarFragment.kt
+++ Unity/app/src/main/java/com/example/u_nity/CalendarFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 
 class CalendarFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +14,15 @@ class CalendarFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+        val view = inflater.inflate(R.layout.fragment_calendar, container, false)
+
+        val events_title = view.findViewById<View>(R.id.events_title)
+        events_title.findViewById<TextView>(R.id.title).text = "Events"
+        events_title.findViewById<TextView>(R.id.subtitle).text = "2 days until next event"
+
+        val button = view.findViewById<ImageButton>(R.id.backButton)
+        button.alpha = 0F
+
+        return view
     }
 }
